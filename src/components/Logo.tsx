ca8924e150c660c1273316fc1322734f765cdd02
Otter Life <otterlife55@gmail.com>
2025-07-10 06:19:53 +0000

    this is what i see

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { cn } from '@/lib/utils';
@@ -6,23 +5,13 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center justify-center gap-2", className)}>
-      {/* Light version of logo, shown on dark backgrounds */}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/logo-light.png"
-        alt="Bio-Harmonize Logo"
-        width={32}
-        height={32}
-        className="hidden h-8 w-8 object-contain dark:block"
-      />
-      {/* Dark version of logo, shown on light backgrounds */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="/logo-dark.png"
         alt="Bio-Harmonize Logo"
         width={32}
         height={32}
-        className="block h-8 w-8 object-contain dark:hidden"
+        className="h-8 w-8 object-contain"
       />
       <span className="text-xl font-semibold font-headline text-foreground">Bio-Harmonize</span>
     </div>
