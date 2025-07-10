c4ac47224a5959106aa75f3bbe8ee986bbaee49b
Otter Life <otterlife55@gmail.com>
2025-07-10 06:16:02 +0000

    <img alt="Bio-Harmonize Logo" width="112" height="32" decoding="async" d

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,31 +1,32 @@
 
+
 "use client";
 
 import { cn } from '@/lib/utils';
-import Image from 'next/image';
 
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center justify-center gap-2", className)}>
       {/* Light version of logo, shown on dark backgrounds */}
-      <Image
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         src="/logo-light.png"
         alt="Bio-Harmonize Logo"
         width={112}
         height={32}
         className="hidden h-8 w-auto object-contain dark:block"
-        priority
       />
       {/* Dark version of logo, shown on light backgrounds */}
-      <Image
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         src="/logo-dark.png"
         alt="Bio-Harmonize Logo"
         width={112}
         height={32}
         className="block h-8 w-auto object-contain dark:hidden"
-        priority
       />
       <span className="text-xl font-semibold font-headline text-foreground">Bio-Harmonize</span>
     </div>
   );
 }
+
