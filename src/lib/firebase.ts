d7d4e4d8262e4cbe6a54cd5ea57d694c9320a42e
Otter Life <otterlife55@gmail.com>
2025-07-14 22:28:55 +0000

    https://bio-harmonize--bio-harmonize.us-central1.hosted.app/
    its workin

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,33 +6,28 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 // This configuration is for your "bio-harmonize" project.
 const firebaseConfig = {
   apiKey: "AIzaSyBtKXyprVuRWmK84yW7gzQU2nWSt3ccLtM",
-  authDomain: "bio-harmonize.firebaseapp.com",
+  authDomain: "bio.joevanniekerk.com",
   projectId: "bio-harmonize",
   storageBucket: "bio-harmonize.appspot.com",
   messagingSenderId: "1033987752579",
-  appId: "1:1033987752579:web:a953cf476632c02af505b2", // Using a valid App ID format
+  appId: "1:1033987752579:web:a953cf476632c02af505b2",
 };
 
 let app: FirebaseApp;
 let auth: Auth;
 let db: Firestore;
-let firebaseEnabled = true;
 
-if (getApps().length === 0) {
-  try {
-    app = initializeApp(firebaseConfig);
-  } catch (e) {
-    console.error("Firebase initialization error", e);
-    firebaseEnabled = false;
-  }
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
 } else {
   app = getApp();
 }
 
-// @ts-ignore - These will be assigned if initialization succeeds
 auth = getAuth(app);
-// @ts-ignore
 db = getFirestore(app);
 
+// We add a single boolean to check if the config is valid,
+// but we no longer check for environment variables which caused the issue.
+const firebaseEnabled = !!firebaseConfig.apiKey;
 
 export { app, auth, db, firebaseEnabled };
