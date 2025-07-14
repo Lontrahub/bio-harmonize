064ca5e0a28cd7719100beb130e2a01e5e8ba220
Otter Life <otterlife55@gmail.com>
2025-07-14 22:50:59 +0000

    fix: add engines to package.json and secure firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 
 // This configuration is for your "bio-harmonize" project.
 const firebaseConfig = {
-  apiKey: "AIzaSyBtKXyprVuRWmK84yW7gzQU2nWSt3ccLtM",
-  authDomain: "bio.joevanniekerk.com",
-  projectId: "bio-harmonize",
-  storageBucket: "bio-harmonize.appspot.com",
-  messagingSenderId: "1033987752579",
-  appId: "1:1033987752579:web:a953cf476632c02af505b2",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 let app: FirebaseApp;
